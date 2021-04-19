9798f0cacf2df708591757ec8e68f2d104a08ef7
Theodosios Melidis <th.melidis@insurance.nbg.gr>
2021-04-19 15:19:43 +0300

    fix: url in data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,23 +11,26 @@ import { SearchRequest } from '../models/search-request';
 import { SearchItem } from '../models/search-response';
 import { UpdateRequest } from '../models/update-request';
 import { UpdateResponse } from '../models/update-response';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
+  private url = environment.baseUrl;
+
   constructor(private http: HttpClient) {}
 
   searchStandingOrder(searchRequest: SearchRequest): Observable<SearchItem[]> {
-    return this.http.post<SearchItem[]>('search/standingOrder', searchRequest);
+    return this.http.post<SearchItem[]>(`${this.url}search/standingOrder`, searchRequest);
   }
 
   searchPolicy(searchPolicyRequest: SearchPolicyRequest): Observable<SearchPolicyResponse> {
-    return this.http.post<SearchPolicyResponse>('/int/search/policy', searchPolicyRequest);
+    return this.http.post<SearchPolicyResponse>(`${this.url}/int/search/policy`, searchPolicyRequest);
   }
 
   create(createRequest: CreateRequest): Observable<CreateResponse> {
-    return this.http.post<CreateResponse>('/int/create', createRequest);
+    return this.http.post<CreateResponse>(`${this.url}/int/create`, createRequest);
   }
 
   // delete(deleteRequest: DeleteRequest): Observable<DeleteResponse> {
@@ -35,10 +38,10 @@ export class DataService {
   // }
 
   searchLinesOfBusiness(): Observable<string[]> {
-    return this.http.get<string[]>('search/linesOfBusiness');
+    return this.http.get<string[]>(`${this.url}search/linesOfBusiness`);
   }
 
   update(updateRequest: UpdateRequest): Observable<UpdateResponse> {
-    return this.http.put<UpdateResponse>('/int/delete', updateRequest);
+    return this.http.put<UpdateResponse>(`${this.url}int/delete`, updateRequest);
   }
 }
