65ee609aa0d99f04183f0281c23caf1dc884176b
Theodosios Melidis <th.melidis@insurance.nbg.gr>
2021-04-19 18:18:20 +0300

    feat: declare search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { SearchStandingOrderComponent } from './search-standing-order/search-standing-order.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchStandingOrderComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
