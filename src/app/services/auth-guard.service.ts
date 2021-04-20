f19057847f268c13aa3c43cd91ed3843d93e7c6a
Theodosios Melidis <th.melidis@insurance.nbg.gr>
2021-04-20 15:07:51 +0300

    feat: add auth guard

---
+++ src/app/services/auth-guard.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable()
+export class AuthGuardService implements CanActivate {
+  constructor(
+    private router: Router,
+    private authService: AuthenticationService
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    debugger;
+    if (!this.authService.isTokenValid()) return true;
+
+    this.router.navigate(['login']);
+    return false;
+  }
+}
