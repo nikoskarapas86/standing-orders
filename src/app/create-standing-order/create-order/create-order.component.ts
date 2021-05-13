4fb6f8864341da6bbe2450bfdff11e4d3b36609b
ΚΑΡΑΠΑΣ ΝΙΚΟΛΑΟΣ <n.karapas@insurance.nbg.gr>
2021-05-13 13:01:29 +0300

    iban created

--- src/app/create-standing-order/create-order/create-order.component.ts
+++ src/app/create-standing-order/create-order/create-order.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { CreateOrderRersponse } from 'src/app/models/create-order-response';
 import { SearchPolicyResponse } from 'src/app/models/search-policy-response';
 import { DataService } from 'src/app/services/data.service';
@@ -12,38 +13,40 @@ import { CreateStandingService } from '../create-standing.service';
 })
 export class CreateOrderComponent implements OnInit {
   creationalForm: FormGroup
-
+  standingOrderID: number;
   constructor(
     private formBuilder: FormBuilder,
     private dataService: DataService,
-    private createStandingService:CreateStandingService 
-    
-    ) { }
+    private router: Router,
+    private createStandingService: CreateStandingService
+
+  ) { }
 
   ngOnInit(): void {
     this.creationalFormGroup()
     this.createStandingService.createOrderResponse$.subscribe((res: CreateOrderRersponse) => {
-      this.fillCreationalForm(res)
+     res? this.fillCreationalForm(res):this.router.navigate(['/home'])
     })
   }
 
   fillCreationalForm(res) {
+    this.standingOrderID = res.id
     for (let item in res) {
       res[item] ? this.creationalForm.controls[item]?.setValue(res[item]) : this.creationalForm.controls[item]?.setValue(null);
     }
   }
   creationalFormGroup() {
     this.creationalForm = this.formBuilder.group({
-      lineOfBusiness: null,
-      policyNo: null,
-      lastName: null,
-      firstName: null,
-      street: null,
-      city: null,
-      postalCode: null,
-      phoneNumber: null,
-      email: null,
-      vatNumber: null
+      lineOfBusiness: [{value: '', disabled: true}],
+      policyNo: [{value: '', disabled: true}],
+      lastName: [{value: '', disabled: true}],
+      firstName: [{value: '', disabled: true}],
+      street: [{value: '', disabled: true}],
+      city: [{value: '', disabled: true}],
+      postalCode: [{value: '', disabled: true}],
+      phoneNumber: [{value: '', disabled: true}],
+      email: [{value: '', disabled: true}],
+      vatNumber: [{value: '', disabled: true}]
     })
   }
 }
