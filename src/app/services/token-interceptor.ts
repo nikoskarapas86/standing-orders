8db4478f6426c97b6bdf2bf45f7387492d313b7d
ΚΑΡΑΠΑΣ ΝΙΚΟΛΑΟΣ <n.karapas@insurance.nbg.gr>
2021-05-25 12:06:01 +0300

    interceptors added

--- src/app/services/token-interceptor.ts
+++ src/app/services/token-interceptor.ts
@@ -20,6 +20,7 @@ export class TokenInterceptor implements HttpInterceptor {
     request = request.clone({
       setHeaders: {
         Authorization: 'Bearer ' + this.authenticationService.token,
+        "Accept-Language":"el"
       },
     });
 
