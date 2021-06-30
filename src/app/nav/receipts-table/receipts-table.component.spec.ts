118df6271443d2b092e407501f456f02403313a8
Theodosios Melidis <th.melidis@insurance.nbg.gr>
2021-06-30 11:27:49 +0300

    feat: add receipt table

---
+++ src/app/nav/receipts-table/receipts-table.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReceiptsTableComponent } from './receipts-table.component';
+
+describe('ReceiptsTableComponent', () => {
+  let component: ReceiptsTableComponent;
+  let fixture: ComponentFixture<ReceiptsTableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReceiptsTableComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReceiptsTableComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
