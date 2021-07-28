ccdb4546d4e6bab458504f7b68e231e33cec71ae
ΜΑΡΙΑ ΦΑΡΑΣΟΠΟΥΛΟΥ <m.faraso@insurance.nbg.gr>
2021-07-28 17:21:28 +0300

    fix: locale in material datepicker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NavComponent } from './nav/nav.component';
-import { MaterialModule } from './material';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { TokenInterceptor } from './services/token-interceptor';
-import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SearchStandingOrderComponent } from './search-standing-order/search-standing-order.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import {
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+  MAT_MOMENT_DATE_FORMATS,
+  MatMomentDateModule,
+  MomentDateAdapter,
+} from '@angular/material-moment-adapter';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { AuthGuardService } from './services/auth-guard.service';
-import { SearchTableComponent } from './search-table/search-table.component';
-import { CommonModule } from '@angular/common';
-import { EditStandingOrderComponent } from './edit-standing-order/edit-standing-order.component';
-import { CreditCardComponent } from './credit-card/credit-card.component';
-import { IbanComponent } from './iban/iban.component';
-import { DeleteListComponent } from './delete-list/delete-list.component';
-import { ModalComponent } from './modal/modal.component';
-import { HomeButtonComponent } from './home-button/home-button.component';
-import { PolicyDetailsComponent } from './policy-details/policy-details.component';
-import { LoaderComponent } from './loader/loader.component';
-import { CreditCardFormComponent } from './credit-card-form/credit-card-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { ClientContainerComponent } from './client-container/client-container.component';
+import { CreateReceiptModalComponent } from './create-receipt-modal/create-receipt-modal.component';
+import { CreditCardComponent } from './credit-card/credit-card.component';
+import { CreditCardFormComponent } from './credit-card-form/credit-card-form.component';
+import { DeleteListComponent } from './delete-list/delete-list.component';
+import { EditStandingOrderComponent } from './edit-standing-order/edit-standing-order.component';
+import { HomeButtonComponent } from './home-button/home-button.component';
+import { HomeComponent } from './home/home.component';
+import { IbanComponent } from './iban/iban.component';
+import { LoaderComponent } from './loader/loader.component';
+import { LoginComponent } from './login/login.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MaterialModule } from './material';
+import { ModalComponent } from './modal/modal.component';
+import { NavComponent } from './nav/nav.component';
+import { PolicyDetailsComponent } from './policy-details/policy-details.component';
 import { ReceiptComponent } from './receipt/receipt.component';
 import { ReceiptsTableComponent } from './receipts-table/receipts-table.component';
+import { SearchStandingOrderComponent } from './search-standing-order/search-standing-order.component';
+import { SearchTableComponent } from './search-table/search-table.component';
+import { TokenInterceptor } from './services/token-interceptor';
 import { UpdateReceiptModalComponent } from './update-receipt-modal/update-receipt-modal.component';
-import { CreateReceiptModalComponent } from './create-receipt-modal/create-receipt-modal.component';
+import localeEl from '@angular/common/locales/el';
 
 @NgModule({
   declarations: [
@@ -37,7 +46,6 @@ import { CreateReceiptModalComponent } from './create-receipt-modal/create-recei
     NavComponent,
     HomeComponent,
     SearchStandingOrderComponent,
-
     SearchTableComponent,
     EditStandingOrderComponent,
     CreditCardComponent,
@@ -62,10 +70,19 @@ import { CreateReceiptModalComponent } from './create-receipt-modal/create-recei
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatMomentDateModule,
     ReactiveFormsModule.withConfig({ warnOnNgModelWithFormControl: 'never' }),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true },
+    { provide: MAT_DATE_LOCALE, useValue: 'el' },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
     AuthGuardService,
   ],
   bootstrap: [AppComponent],
--- src/app/search-standing-order/search-standing-order.component.ts
+++ src/app/search-standing-order/search-standing-order.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import * as moment from 'moment';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import 'moment/locale/el';
 
-import { ModalComponent } from '../modal/modal.component';
-import { LineOfBusiness } from '../models/line-of-business';
-import { SearchRequest } from '../models/search-request';
+import * as moment from 'moment';
+
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SearchItem, SearchResponse } from '../models/search-response';
+
 import { DataService } from '../services/data.service';
 import { DestroyService } from '../services/destroy.service';
-import { SearchService } from '../services/search.service';
-import { DateAdapter } from '@angular/material/core';
-import { Router } from '@angular/router';
+import { LineOfBusiness } from '../models/line-of-business';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
+import { Observable } from 'rxjs';
 import { PaymentType } from '../models/payment-type';
+import { Router } from '@angular/router';
+import { SearchRequest } from '../models/search-request';
+import { SearchService } from '../services/search.service';
+import { takeUntil } from 'rxjs/operators';
+
+// import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+// import {
+//   MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+//   MAT_MOMENT_DATE_FORMATS,
+//   MomentDateAdapter,
+// } from '@angular/material-moment-adapter';
+// { provide: MAT_DATE_LOCALE, useValue: 'el-EL' },
+// {
+//   provide: DateAdapter,
+//   useClass: MomentDateAdapter,
+//   deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+// },
+// { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
 
 @Component({
   selector: 'app-search-standing-order',
@@ -30,35 +46,31 @@ export class SearchStandingOrderComponent implements OnInit {
   standingOrders: SearchItem[];
   searchBtnDisabled: boolean = false;
   searchId: string;
-  standingOrders$: Observable<any[]>;  
+  standingOrders$: Observable<any[]>;
   minDate = moment().subtract(3, 'months').toDate();
- 
+
   constructor(
     private formBuilder: FormBuilder,
     public dataService: DataService,
     private searchService: SearchService,
     public dialog: MatDialog,
     private readonly destroy$: DestroyService,
-    private _adapter: DateAdapter<any>,
     private router: Router
   ) {}
 
   ngOnInit(): void {
-  
+    moment.locale('el');
     this.buildFormGroup();
     this.linesOfBusinesses$ = this.dataService.lineOfbusinesses$;
     this.paymentTypes$ = this.dataService.getPaymentTypes();
     this.searchService.getIsDeleteCalled.subscribe(res => {
       if (res) {
-      
         this.submit();
         this.searchService.isDeleteCalled = false;
       }
     });
-    this._adapter.setLocale('el');
 
     if (this.dataService.searchRequest) {
-     
       this.fetchSearchResults();
       this.searchForm = this.dataService.searchForm;
     }
@@ -81,7 +93,6 @@ export class SearchStandingOrderComponent implements OnInit {
   }
 
   private createSubmitRequest(): void {
-  
     const tempStartDate = this.searchForm.get('payDateFrom').value;
     const tempEndDate = this.searchForm.get('payDateTo').value;
     const startDate = tempStartDate ? moment(tempStartDate).format('DD/MM/YYYY') : null;
@@ -106,30 +117,28 @@ export class SearchStandingOrderComponent implements OnInit {
 
   submit(): void {
     this.dataService.setStandingOrdersSubject(undefined);
-    this.dataService.resultsLoadingSubject.next(true)
+    this.dataService.resultsLoadingSubject.next(true);
     this.searchBtnDisabled = true;
     this.dataService
       .searchStandingOrder(this.dataService.searchRequest)
       .pipe(takeUntil(this.destroy$))
       .subscribe(
         (res: SearchResponse) => {
-         
           this.searchId = res.searchId;
           this.dataService.setStandingOrdersSubject(res);
           this.standingOrders$ = this.dataService.standingOrders$;
         },
         error => {
-        this.errorHandler(error)
+          this.errorHandler(error);
         },
         () => {
           this.searchBtnDisabled = false;
-          this.dataService.resultsLoadingSubject.next(false)
+          this.dataService.resultsLoadingSubject.next(false);
         }
       );
   }
 
-
-  errorHandler(error){
+  errorHandler(error) {
     this.dataService.setStandingOrdersSubject(undefined);
     this.searchBtnDisabled = false;
     this.standingOrders$ = this.dataService.standingOrders$;
